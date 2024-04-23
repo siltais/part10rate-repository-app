c1e59975314779d5311996e529dc2505ed487cce
Kristaps <53013669+siltais@users.noreply.github.com>
2024-04-23 12:11:07 +0300

    10.17

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -11,13 +11,10 @@ const styles = StyleSheet.create({
   }
 });
 
-const ItemSeparator = () => <View style={styles.separator} />;
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
-    : [];
+  ? repositories.edges.map(edge => edge.node)
+  : [];
 
   return (
     <SafeAreaView style={styles.listContainer}>
@@ -31,6 +28,13 @@ const RepositoryList = () => {
       />
     </SafeAreaView>
   );
+};
+
+const ItemSeparator = () => <View style={styles.separator} />;
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+  
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
